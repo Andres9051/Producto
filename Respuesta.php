6de5bf53b0ad677e775675f7224867e4ef696893
Andres9051 <aesh1509@gmail.com>
2024-08-08 15:21:39 -0500

    Create Respuesta.php

---
+++ Respuesta.php
@@ -0,0 +1,9 @@
+<?php
+$Producto_1= $_POST ['Producto_1'];
+$Producto_2= $_POST ['Producto_2'];
+$Producto_3= $_POST ['Producto_3'];
+echo "La identificacion del Empleado es: ".Identificacion;
+echo "Los nombres y apellidos del empleado son: ".NomApellidos;
+echo "El cargo del empleado es: ".Cargo;
+echo "El salario del empleado es: ".Salario;
+?>
