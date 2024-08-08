efe482500801720b8c4596750644c17deec7b020
Andres9051 <aesh1509@gmail.com>
2024-08-08 15:30:17 -0500

    Update Respuesta.php

--- Respuesta.php
+++ Respuesta.php
@@ -2,8 +2,9 @@
 $Producto_1= $_POST ['Producto_1'];
 $Producto_2= $_POST ['Producto_2'];
 $Producto_3= $_POST ['Producto_3'];
-echo "La identificacion del Empleado es: ".Identificacion;
-echo "Los nombres y apellidos del empleado son: ".NomApellidos;
-echo "El cargo del empleado es: ".Cargo;
-echo "El salario del empleado es: ".Salario;
+$Producto_1 $Producto_2 $Producto_3= $_POST ["Suma"];
+echo "El primer producto y su precio es: ".Producto_1;
+echo "El segundo producto y su precio es: ".Producto_2;
+echo "El tercer producto y su precio es: ".Producto_3;
+echo "La suma de los precios delproduto es: ".Suma;
 ?>
